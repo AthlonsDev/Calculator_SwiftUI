0e95d14dd5265cbaf40239eb7552ab4187c14c82
Andrea Sanna <athlons91@gmail.com>
2021-04-21 18:39:34 +0100

    Initial Commit

---
+++ Calculator_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator_SwiftUI
+//
+//  Created by Athlosn90 on 21/04/2021.
+//  Copyright © 2021 AndreaSanna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
